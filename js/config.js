5e3753dface44d2231fc132196c7a1fda9c77332
riad_riad <132849221+riad-riad0789@users.noreply.github.com>
2026-02-14 00:56:20 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_j1xq3Cl9C', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '20qur4h1jm7e5o6qqrl6vopu3v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_gkjAyDnsc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '31dnuoputa1ven2v9djvp1f4ib', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
